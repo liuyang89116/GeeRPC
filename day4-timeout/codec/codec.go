8d1f782e671436bd9d1c72edcd9b9145fb2ea0fa
yang <liuyang89116@qq.com>
2023-01-02 21:53:14 -0800

    init day4

---
+++ day4-timeout/codec/codec.go
@@ -0,0 +1,32 @@
+package codec
+
+import "io"
+
+type Header struct {
+	ServiceMethod string // format "Service.Method"
+	Seq           uint64 // sequence number chosen by client
+	Error         string
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(io.ReadWriteCloser) Codec // 类似工厂模式，但返回的不是实例，而是构造函数
+
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "application/json" // not implemented
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
+}
